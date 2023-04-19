872c4978bc409a2887633cb1d2f394cc408f4dc0
Liberty Vasquez <201980193@psu.palawan.edu.ph>
2023-04-19 23:21:39 +0800

    05pset done

--- web2/fav.php
+++ web2/fav.php
@@ -2,6 +2,11 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
+        if(isset($_POST['username'])){
+            $color=htmlentities($_POST['color']);
+            $dish=htmlentities($_POST['dish']);
+            $username=htmlentities($_POST['username']);
+            echo "you really enjoy $dish -especially with a nice $color wine"; }
         
     ?>
 </body>
--- web3/calc.php
+++ web3/calc.php
@@ -2,18 +2,53 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
+        if(isset($_POST['val1'])){
+        if(isset($_POST['val2'])){
+        $val1=htmlentities($_POST['val1']);
+        $val2=htmlentities($_POST['val2']);
+        $calc=htmlentities($_POST['calc']);
         # test if input is_numeric
-        if(1){
-            # test if calc is not null
-            if(1){
-                # switch statement for value of calc
-                switch(1){
-                    
-                }
-                echo "Calculation result: $result";
-            }
+        function is_numeric_($x) {
+        if(is_numeric($x)){
+            echo "$x is a integer/number <br>";}
+            else 
+            {echo "$x is not a number, please retry";}
         }
+            is_numeric_($val1);
+            is_numeric_($val2);
+
+           
+
+            
+            
+            
+            # test if calc is not null
+            if(is_null($calc)){
+                $result=0;
+                # switch statement for value of calc
+                if(isset($_POST['calc'])){
+                switch($calc){
+                    case add:
+                        $result= $val1 + $val2;
+                        break;
+                    case Subtract:
+                        $result= $val1 - $val2;
+                        break; 
+                    case Multiply:
+                        $result= $val1 * $val2;
+                        break;
+                    case Divide:
+                        $result= $val1 / $val2;
+                        break;
+                    }
+                echo "Calculation result: $result"; 
+                }}}
+        
         else{ echo "Invalid entry - please retry"; }
+            
+                is_null($calc); 
+            
+       
     ?>
 </body>
 </html>
