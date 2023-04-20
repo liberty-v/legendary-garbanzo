d20b3c38497d12df0a25edc6e735c283d84e27d7
Liberty Vasquez <201980193@psu.palawan.edu.ph>
2023-04-20 12:37:36 +0800

    web3 done

--- web3/calc.php
+++ web3/calc.php
@@ -6,13 +6,13 @@
         if(isset($_POST['val2'])){
         $val1=htmlentities($_POST['val1']);
         $val2=htmlentities($_POST['val2']);
-        $calc=htmlentities($_POST['calc']);
+        
         # test if input is_numeric
         function is_numeric_($x) {
         if(is_numeric($x)){
             echo "$x is a integer/number <br>";}
             else 
-            {echo "$x is not a number, please retry";}
+            {echo "$x is not a number, please retry <br>";}
         }
             is_numeric_($val1);
             is_numeric_($val2);
@@ -20,33 +20,33 @@
            
 
             
-            
-            
+            if(isset($_POST['calc']))
+            $calc=htmlentities($_POST['calc']);
             # test if calc is not null
-            if(is_null($calc)){
+            if(!empty($calc)){
                 $result=0;
                 # switch statement for value of calc
-                if(isset($_POST['calc'])){
+                
+                {
                 switch($calc){
-                    case add:
+                    case 'add':
                         $result= $val1 + $val2;
                         break;
-                    case Subtract:
+                    case 'sub':
                         $result= $val1 - $val2;
                         break; 
-                    case Multiply:
+                    case 'mul':
                         $result= $val1 * $val2;
                         break;
-                    case Divide:
+                    case 'div':
                         $result= $val1 / $val2;
                         break;
                     }
                 echo "Calculation result: $result"; 
-                }}}
+                }}}}
         
         else{ echo "Invalid entry - please retry"; }
-            
-                is_null($calc); 
+             
             
        
     ?>
